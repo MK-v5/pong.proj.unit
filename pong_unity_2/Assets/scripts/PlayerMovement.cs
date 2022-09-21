1d9d6ec97550434d56ecfdd9bdb5d4d41a256626
MK-v5 <mksomna@st.noorderpoort.nl>
2022-09-21 14:03:12 +0200

    ver1.2 pong
    made rigidbody2d function properly

--- pong_unity_2/Assets/scripts/PlayerMovement.cs
+++ pong_unity_2/Assets/scripts/PlayerMovement.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int PlayerNumber = 1;
-    // Update is called once per frame
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0.0f;
+    }
     void Update()
     {
         if (PlayerNumber == 1)
         {
-            transform.Translate(new Vector3(0, Input.GetAxis("player1") * Time.deltaTime * 6, 0));
+            transform.Translate(new Vector3(0, Input.GetAxis("player1") * Time.deltaTime * 10, 0));
         }
         
         if (PlayerNumber == 2)
         {
-            transform.Translate(new Vector3(0, Input.GetAxis("player2") * Time.deltaTime * 6, 0));
+            transform.Translate(new Vector3(0, Input.GetAxis("player2") * Time.deltaTime * 10, 0));
         }
     }
 }
